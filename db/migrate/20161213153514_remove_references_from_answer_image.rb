cb494ed3245312695ad8ae90c27d4cd323fde891
Andrés Da Viá <andresi.davia@gmail.com>
2016-12-13 09:54:16 -0600

    Added admin flag to users

--- db/migrate/20161213153514_remove_references_from_answer_image.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReferencesFromAnswerImage < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :answer_images, :image, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210183129) do
+ActiveRecord::Schema.define(version: 20161213154714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,8 +162,10 @@ ActiveRecord::Schema.define(version: 20161210183129) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "api_key"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
+    t.string   "image"
   end
 
   add_foreign_key "answer_dates", "answers"

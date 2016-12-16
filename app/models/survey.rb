e21a7a0f248fb646d5108ec2634e1110338e229b
Andrés Da Viá <andresi.davia@gmail.com>
2016-12-16 15:50:06 -0600

    Add new validation to survey model questions presence

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,7 +4,8 @@ class Survey < ApplicationRecord
   has_many :submissions, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 4, maximum: 250 }
-
+  validates :questions,
+    presence: { message: "You must add at least one question to the survey." }
   validates_associated :questions
 
   accepts_nested_attributes_for :questions, allow_destroy: true

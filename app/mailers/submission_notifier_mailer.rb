8190797c68014e1bf61a148842d8ec993e1ffc92
Andrés Da Viá <andresi.davia@gmail.com>
2017-01-06 10:58:33 -0600

    Change email reciepient

--- app/mailers/submission_notifier_mailer.rb
+++ app/mailers/submission_notifier_mailer.rb
@@ -1,7 +1,7 @@
 class SubmissionNotifierMailer < ApplicationMailer
   def rating_notifier(submission)
     @submission = submission
-    mail(to: "andresi.davia@gmail.com",
+    mail(to: "hugo@socialastronauts.com",
       subject: "Rating notification - #{@submission.survey.name}")
   end
 end

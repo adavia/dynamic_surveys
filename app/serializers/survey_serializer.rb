e7c79d4b12d2e7b2a1ea3245c000eb7b1c47c061
Andrés Da Viá <andresi.davia@gmail.com>
2017-01-10 11:44:16 -0600

    Add user image to survey api show

--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -1,6 +1,10 @@
 class SurveySerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :avatar, :questions_count, :created_at
+  attributes :id, :name, :description, :avatar, :questions_count, :user_image, :created_at
 
   belongs_to :customer
   has_many :questions
+
+  def user_image
+    current_user.image
+  end
 end

b4bc9f89abc467d01ca79ca23510c6bbfd42ade7
Andrés Da Viá <andresi.davia@gmail.com>
2017-04-06 11:32:05 -0500

    Debug error mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "dynamic_surveys_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

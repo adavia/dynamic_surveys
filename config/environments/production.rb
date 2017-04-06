5c13fd9f60c85d669eef1741340b7f4dcf4cf65c
Andrés Da Viá <andresi.davia@gmail.com>
2017-04-06 16:53:11 -0500

    Change pdf report partials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "dynamic_surveys_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   # Mailgun setup
   ActionMailer::Base.delivery_method = :smtp
-  host = "http://socialsurveys.net"
+  host = "https://sleepy-ravine-58079.herokuapp.com"
 
   config.action_mailer.asset_host = host
 
